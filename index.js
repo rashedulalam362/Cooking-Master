6ed23f7e7e7e46520e875619b3911d8607e3a47c
rashedulalam362 <rashedulalam362@gmail.comgit config --global user.name rashedulalam362git commit -mfirst git config --global user.email rashedulalam362@gmail.com>
2021-02-09 23:50:02 +0600

    updated

--- index.js
+++ index.js
@@ -40,44 +40,25 @@ document.getElementById('btn_search').addEventListener('click',()=>{
     })
   
 
+   
 
-    const  getDetail=(mealImage,mealName)=>{
+ const  getDetail=(mealImage,mealName)=>{
 
    const url=`https://www.themealdb.com/api/json/v1/1/list.php?i=${mealImage}${mealName}`
    fetch(url)
-   .then(data=>{
-    console.log(data);
-    
- for (let i = 0; i < data.meals[i].length; i++) {
-   const meal = data.meals[i];
-
-  
-   const mealImage=data.meals[i].strMealThumb;
-   
-   const mealName=data.meals[i].strMeal;
-  
-   document.getElementById('search_result').innerHTML +=` meaLImage: <img src=${mealImage}>
-    
-   mealName:${mealName}
-   
-   
+   .then(res=>res.json())
+   .then(data=> renderMealInfo(data.meals[0]))
+    // console.log(data);
+    //     // const ingredients =data.meals[0].strIngredient;
+    //     // const ingredientsDetail =data.meals[0].strDescription;
  
-    `
-
- }
-
-
-
-
-
-})
     
     }
     
-// const renderMealInfo=strIngredient=>{
-//     console.log(strIngredient);
-// const mealDiv=document.getElementById("mealDetail")
-// // mealDiv.innerHTML=``
+const renderMealInfo=Ingredient=>{
+    console.log(Ingredient);
+   const mealDiv=document.getElementById('meal_detail');
+   mealDiv.innerHTML=`${Ingredient.strIngredient}`
 
 
-// }
+}
